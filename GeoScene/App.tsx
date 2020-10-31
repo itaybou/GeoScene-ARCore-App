9d2924427f1b7f1f574b4af67cf2e3aba634237b
itay <itaibu@gmail.com>
2020-11-01 00:10:53 +0200

    Commenting changes

--- GeoScene/App.tsx
+++ GeoScene/App.tsx
@@ -1,10 +1,5 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
+ * GeoScene App starting page
  * @format
  */
 
