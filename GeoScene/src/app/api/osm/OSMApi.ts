2b942b9c97e99f84e5a9e6d9c119c403471d4ce1
LiorHassan <liorhass@post.bgu.ac.il>
2021-03-31 02:16:56 +0300

    add location functions

--- GeoScene/src/app/api/osm/OSMApi.ts
+++ GeoScene/src/app/api/osm/OSMApi.ts
@@ -4,7 +4,8 @@ const builder = require('xmlbuilder');
 const xmlBuilder = builder.create('osm', { encoding: 'utf-8' });
 
 export const getPermissions = async () => {
-  const details = await authManager.makeRequest('osm', 'api/0.6/permissions', {
+  const details = await authManager.makeRequest('osm', 'api/0.6/permissions', false, 
+  {
     method: 'GET',
   });
   return details;
@@ -17,13 +18,19 @@ export const getPermissions = async () => {
 //    return details;
 //  };
 
+export const addNewLocation = async (lat, lon, name, description) => {
+  const csID = await createChangeset();
+  const nodeID = await createNode(csID, lat, lon, name, description);
+  console.log(nodeID);
+};
+
 export const createChangeset = async () => {
   //   const perm = await getPermissions();
   const xml = xmlBuilder
     .ele('changeset')
     .ele('tag', { k: 'created_by', v: 'GeoScene' })
     .up()
-    .ele('tag', { k: 'comment', v: 'Just adding some streetnames' })
+    .ele('tag', { k: 'comment', v: 'adding new node' })
     .end({ pretty: true, allowEmpty: false });
   //   console.log(perm.data.osm.permissions);
   const id = await authManager.makeRequest(
@@ -39,5 +46,28 @@ export const createChangeset = async () => {
     },
   );
 
+  return id;
+};
+
+
+export const createNode = async (csID:string, lat:string, lon:string, name:string, description) => {
+  const xml = xmlBuilder
+    .ele('node', { k: 'cangeset', v: csID }, { k: 'lat', v: lat }, { k: 'lon', v: lon } )
+    .ele('tag', { k: 'name', v: name })
+    .up()
+    .ele('tag', { k: 'description', v: description })
+    .end({ pretty: true, allowEmpty: false });
+  const id = await authManager.makeRequest(
+    'osm',
+    'api/0.6/node/create',
+    true,
+    {
+      method: 'PUT',
+      headers: { 'Content-Type': 'text/xml' },
+      params: {
+        body: xml,
+      },
+    },
+  );
   return id;
 };
